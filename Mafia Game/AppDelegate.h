48f5dd6fcf4ce42bbd133665ac277922857a0416
zak123 <zacharym123@gmail.com>
2015-03-23 10:43:43 -0700

    Initial Commit

---
+++ Mafia Game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mafia Game
+//
+//  Created by Zachary Mallicoat on 3/23/15.
+//  Copyright (c) 2015 cghcapital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mafia Game/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mafia Game
+//
+//  Created by Zachary Mallicoat on 3/23/15.
+//  Copyright (c) 2015 cghcapital. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
